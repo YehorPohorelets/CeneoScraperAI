0b7ed08e44432e04a52e958354fd9e8bb9d24e2b
en-013-17 <en-013-17@gumiforest.uek.krakow.pl>
2022-05-31 13:02:44 +0200

    2022-31-05 YP

---
+++ analyzer.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+
+print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep="/n")
+
+product_id = input("Please enter a product id: ")
+
+opinions = pd.read_json(f"opinions/{product_id}.json")
+
+opinions_count = len(opinions)
+pros_count = opinions["pros"].map(bool).sum()
+cons_count = opinions["cons"].map(bool).sum()
+average_score = opinions["score"].mean().round(2)
+
+print(average_score)
+
+stars_recommendation = pd.crosstab(opinions["score"], opinions["rcmd"], dropna=False)
+print(stars_recommendation)
---
+++ scraper.py
@@ -0,0 +1,79 @@
+from googletrans import Translator
+from bs4 import BeautifulSoup
+import requests
+import json
+
+def get_element(parent, selector, attribute = None, return_list = False):
+    try:
+        if return_list:
+            return ", ".join([item.text.strip() for item in parent.select(selector)])
+        if attribute:
+            return parent.select_one(selector)[attribute]
+        return parent.select_one(selector).text.strip()
+    except (AttributeError, TypeError):
+        return None 
+
+dest = "en"
+src = "pl"
+translator = Translator
+
+product_id = input("Please, enter the product id: ")
+url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+
+def translate(text, src=src, dest=dest):
+    try:
+        return translator.translate(text, src=src, dest=dest).text
+    except (AttributeError, TypeError):
+        print("Error")
+        return ""
+
+
+
+all_opinions = []
+while (url):
+    response = requests.get(url)
+    page_dom = BeautifulSoup(response.text, "html.parser")
+    opinions = page_dom.select("div.js_product-review") 
+
+
+
+
+    opinion_elements = {
+        "author": ["span.user-post__author-name"],
+        "rcmd": ["span.user-post__author-recomendation > em"],
+        "score": ["span.user-post__score-count"],
+        "content": ["div.user-post__text"],
+        "posted_on": ["span.user-post__published > time:nth-child(1)", "datetime"],
+        "bought_on": ["span.user-post__published > time:nth-child(2)", "datetime"],
+        "usefull_for": ["button.vote-yes > span"],
+        "useless_for": ["button.vote-no > span"],
+        "pros": ["div.review-feature__title--positives ~ div.review-feature__item", None, True],
+        "cons": ["div.review-feature__title--negatives ~ div.review-feature__item", None, True]
+    }
+
+    # Transaltors are trolls, man
+    dest="en"
+    src="pl"
+    translator = Translator()    
+    for opinion in opinions:
+        single_opinion = {
+            key: get_element(opinion, *values)
+            for key, values in opinion_elements.items()
+        }
+        single_opinion["opinion_id"] = opinion["data-entry-id"]
+        single_opinion["rcmd"] = True if single_opinion["rcmd"]  == "Polecam" else False if single_opinion["rcmd"]  == "Nie polecam" else None
+        single_opinion["score"] = float(single_opinion["score"].split("/")[0].replace(",", "."))
+        single_opinion["usefull_for"] = int(single_opinion["usefull_for"])
+        single_opinion["useless_for"] = int(single_opinion["useless_for"])
+        single_opinion["content_en"] = translate(single_opinion["content"]) if single_opinion["content"] else ""
+        single_opinion["pros_en"] = translate(single_opinion['pros']) if single_opinion["pros"] else ""
+        single_opinion["cons_en"] = translate(single_opinion['cons']) if single_opinion["cons"] else ""
+        all_opinions.append(single_opinion)
+    
+    try:
+        url = "https://www.ceneo.pl"+page_dom.select_one("a.pagination__next")["href"]
+    except TypeError:
+        url = None
+
+with open(f"opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
